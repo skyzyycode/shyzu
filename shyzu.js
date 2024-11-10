bc590c5dfb52910c811948ae74830c6f4ea3ad3c
MannHost <146666179+MannOffc@users.noreply.github.com>
2024-11-10 08:36:23 +0700

    Update shyzu.js

--- shyzu.js
+++ shyzu.js
@@ -12,22 +12,83 @@
 **/
 
 require("./lib/config.js")
-var { axios, JavaScriptObfuscator, fetch, fs, chalk, baileys } = require("./lib/module.js")
+var { axios, JavaScriptObfuscator, fetch, fs, chalk, baileys, execSync, util } = require("./lib/module.js")
 var { watchFile, unwatchFile, readFileSync } = fs
-var { generateWAMessageFromContent, proto } = baileys
+var { generateWAMessageFromContent, getContentType, proto } = baileys
+let cp = execSync
+let { promisify } = util
+let exec = promisify(cp.exec).bind(cp)
 
 module.exports = async (shyzu, m) => {
+    try {
     let Read = async (shyzu, jid, messageId) => {
         await shyzu.readMessages([{ remoteJid: jid, id: messageId, participant: null }]);
     }
     
-    if (!m.message) return;
+    if (!m) return m
+    let M = proto.WebMessageInfo
+    if (m.key) {
+        m.id = m.key.id
+        m.isBaileys = m.id.startsWith('BAE5') && m.id.length === 16
+        m.chat = m.key.remoteJid
+        m.fromMe = m.key.fromMe
+        m.isGroup = m.chat.endsWith('@g.us')
+        m.sender = shyzu.decodeJid(m.fromMe && shyzu.user.id || m.participant || m.key.participant || m.chat || '')
+        if (m.isGroup) m.participant = shyzu.decodeJid(m.key.participant) || ''
+    }
+    
+    if (m.message) {
+        m.mtype = getContentType(m.message)
+        m.msg = (m.mtype == 'viewOnceMessage' ? m.message[m.mtype].message[getContentType(m.message[m.mtype].message)] : m.message[m.mtype])
+        m.body = m.message.conversation || m.msg.caption || m.msg.text || (m.mtype == 'listResponseMessage') && m.msg.singleSelectReply.selectedRowId || (m.mtype == 'buttonsResponseMessage') && m.msg.selectedButtonId || (m.mtype == 'viewOnceMessage') && m.msg.caption || m.text
+        let quoted = m.quoted = m.msg.contextInfo ? m.msg.contextInfo.quotedMessage : null
+        m.mentionedJid = m.msg.contextInfo ? m.msg.contextInfo.mentionedJid : []
+        if (m.quoted) {
+            let type = getContentType(quoted)
+			m.quoted = m.quoted[type]
+            if (['productMessage'].includes(type)) {
+				type = getContentType(m.quoted)
+				m.quoted = m.quoted[type]
+			}
+            if (typeof m.quoted === 'string') m.quoted = {
+				text: m.quoted
+			}
+            m.quoted.mtype = type
+            m.quoted.id = m.msg.contextInfo.stanzaId
+			m.quoted.chat = m.msg.contextInfo.remoteJid || m.chat
+            m.quoted.isBaileys = m.quoted.id ? m.quoted.id.startsWith('BAE5') && m.quoted.id.length === 16 : false
+			m.quoted.sender = shyzu.decodeJid(m.msg.contextInfo.participant)
+			m.quoted.fromMe = m.quoted.sender === (shyzu.user && shyzu.user.id)
+            m.quoted.text = m.quoted.text || m.quoted.caption || m.quoted.conversation || m.quoted.contentText || m.quoted.selectedDisplayText || m.quoted.title || ''
+			m.quoted.mentionedJid = m.msg.contextInfo ? m.msg.contextInfo.mentionedJid : []
+            let vM = m.quoted.fakeObj = M.fromObject({
+                key: {
+                    remoteJid: m.quoted.chat,
+                    fromMe: m.quoted.fromMe,
+                    id: m.quoted.id
+                },
+                message: quoted,
+                ...(m.isGroup ? { participant: m.quoted.sender } : {})
+            })
+
+            m.quoted.delete = () => shyzu.sendMessage(m.quoted.chat, { delete: vM.key })
+
+            m.quoted.copyNForward = (jid, forceForward = false, options = {}) => shyzu.copyNForward(jid, vM, forceForward, options)
+
+            m.quoted.download = () => shyzu.downloadMediaMessage(m.quoted)
+        }
+    }
+    if (m.msg.url) m.download = () => shyzu.downloadMediaMessage(m.msg)
+    m.text = m.msg.text || m.msg.caption || m.message.conversation || m.msg.contentText || m.msg.selectedDisplayText || m.msg.title || ''
+
+	m.copyNForward = (jid = m.chat, forceForward = false, options = {}) => shyzu.copyNForward(jid, m, forceForward, options)
+	
     const message = m.message;
     const isGroup = m.key.remoteJid.endsWith('@g.us');
     const isName = m.pushName || 'no name';
     const isFrom = m.key.remoteJid;
     const isSender = isGroup ? (m.key.participant ? m.key.participant : m.participant) : m.key.remoteJid;
-    const isOwner = global.owner.includes(m.sender);
+    const isOwner = global.owner.includes(isSender);
     const groupMetadata = isGroup ? await shyzu.groupMetadata(isFrom) : {}; 
     
     const participants = isGroup ? groupMetadata.participants : '';
@@ -48,17 +109,18 @@ module.exports = async (shyzu, m) => {
     if (!prefix.some(p => body.startsWith(p))) return;
     const [command, ...args] = body.slice(prefix.find(p => body.startsWith(p)).length).trim().split(/ +/);
     const text = args.join(' ');
+    const budy = (typeof text == 'string' ? text : '')
     
     var cmd = prefix + command
 
     await Read(shyzu, m.key.remoteJid, m.key.id);
     
     let x = chalk.bold.cyan("[ Message Shyzu ]");
-    x += chalk.cyan(`\nFrom: ${isSender}`)
-    x += chalk.green(`\nCommand: ${command + " " + text}`)
+    x += chalk.cyan("\nᕐ⁠ᐷ From: ")
+    x += chalk.bold.white(isSender)
+    x += chalk.cyan("\nᕐ⁠ᐷ Command: ")
+    x += chalk.bold.white("." + command + " " + text)
     console.log(x)
-    
-    m.chat = m.key.remoteJid
     
     m.reply = (text) => {
         shyzu.sendMessage(m.key.remoteJid, { text }, { quoted: m });
@@ -91,7 +153,7 @@ module.exports = async (shyzu, m) => {
         if (!text) return m.reply("Masukan pertanyaan!")
         try {
         var { data } = await axios({
-            "method": "POST",
+            "method": "GET",
             "url": "https://hercai.onrender.com/v3/hercai",
             "params": {
                 "question": text
@@ -138,19 +200,19 @@ module.exports = async (shyzu, m) => {
         })
         let { id, region, title, play, duration, play_count, comment_count, share_count, download_count, collect_count, author } = data.result
         let { nickname } = author
-        var x = `┌─⊷ *T I K T O K*`
-        x += `\n${xicon} *Author:* ${nickname}`
-        x += `\n${xicon} *ID:* ${id}`
-        x += `\n${xicon} *Region:* ${region}`
-        x += `\n${xicon} *Title:* ${title}`
-        x += `\n${xicon} *Duration:* ${duration + " Seconds"}`
-        x += `\n${xicon} *Play Count:* ${format(play_count)}`
-        x += `\n${xicon} *Comment Count:* ${format(comment_count)}`
-        x += `\n${xicon} *Share Count:* ${format(share_count)}`
-        x += `\n${xicon} *Download Count:* ${format(download_count)}`
-        x += `\n${xicon} *Collect Count:* ${format(collect_count)}`
-        x += `\n└──────────`
-        shyzu.sendMessage(m.chat, { video: { url: play }, fileLength: 10630044057600000000000000000000000000000000000000000000000000, caption: x }, { quoted: m })
+        var x_ = `┌─⊷ *T I K T O K*`
+        x_ += `\n${xicon} *Author:* ${nickname}`
+        x_ += `\n${xicon} *ID:* ${id}`
+        x_ += `\n${xicon} *Region:* ${region}`
+        x_ += `\n${xicon} *Title:* ${title}`
+        x_ += `\n${xicon} *Duration:* ${duration + " Seconds"}`
+        x_ += `\n${xicon} *Play Count:* ${format(play_count)}`
+        x_ += `\n${xicon} *Comment Count:* ${format(comment_count)}`
+        x_ += `\n${xicon} *Share Count:* ${format(share_count)}`
+        x_ += `\n${xicon} *Download Count:* ${format(download_count)}`
+        x_ += `\n${xicon} *Collect Count:* ${format(collect_count)}`
+        x_ += `\n└──────────`
+        shyzu.sendMessage(m.chat, { video: { url: play }, fileLength: 10630044057600000000000000000000000000000000000000000000000000, caption: x_ }, { quoted: m })
         } catch ({ message }) {
         m.reply(message)
         }
@@ -167,15 +229,129 @@ module.exports = async (shyzu, m) => {
         }
         break **/
         
+        case "ngl": {
+        let c = text.split("|")
+        let username = c[0]
+        let message = c[1]
+        if (!username) return m.reply("Masukan usernamenya, Contoh *.ngl* mann|halo")
+        if (!message) return m.reply("Masukan messagenya, Contoh *.ngl* mann|halo")
+        try {
+        await axios({
+            method: "GET",
+            url: "https://api-nodex.vercel.app",
+            params: {
+                username,
+                message
+            }
+        })
+        m.reply("Pesan " + message + " berhasil terkirim ke " + username)
+        } catch ({ message }) {
+        m.reply(message)
+        }
+        }
+        break
+        
+        case "exec": {
+        if (!isOwner) return m.reply("Khusus owner hehe :3")
+        m.reply('🐾Executing...')
+        let o
+        try {
+        o = await exec(command.trimStart()  + ' ' + text.trimEnd())
+        } catch (e) {
+        o = e
+        } finally {
+        let { stdout, stderr } = o
+        if (stdout.trim()) m.reply(stdout)
+        if (stderr.trim()) m.reply(stderr)
+        }
+        }
+        break
+        
+        case ">": {
+        if (!isOwner) return m.reply("Khusus owner hehe :3")
+        try {
+        let evaled = await eval(text)
+        if (typeof evaled !== 'string') evaled = require('util').inspect(evaled)
+        await m.reply(evaled)
+        } catch (err) {
+        m.reply(String(err))
+        }
+        }
+        break
+        
+        /** case "auto-ai": {
+        if (!auto_ai) {
+        shyzu.auto_ai = shyzu.auto_ai ? shyzu.auto_ai : {};
+        shyzu.auto_ai[isSender] = {
+            status: true
+        }
+        m.reply("Mengaktifkan auto ai")
+        m.reply("Jika ingin menonaktifkannya silahkan ketik *.auto-ai* sekali lagi")
+        } else {
+        delete shyzu.auto_ai[isSender]
+        m.reply("Menonaktifkan auto ai")
+        }
+        }
+        break **/
+        
+        case "soundcloud": {
+        if (!text) return m.reply("Masukan judulnya, Contoh *.soundcloud* dear god");
+        try {
+        var { data: dataSearch } = await axios({
+            "method": "GET",
+            "url": "https://api-nodex.vercel.app/soundcloud/search",
+            "params": {
+                "q": text
+            }
+        })
+    
+        let { result: resultSearch } = dataSearch
+        // let { link, title } = resultSearch[Math.floor(Math.random() * resultSearch.length)]
+        let { link, title } = resultSearch[0]
+    
+        var { data: dataDownload } = await axios({
+            "method": "GET",
+            "url": "https://api-nodex.vercel.app/soundcloud/download",
+           "params": {
+                "url": link
+            }
+        })
+    
+        let { result: resultDownload } = dataDownload
+    
+        shyzu.sendMessage(m.chat, { audio: { url: resultDownload.download }, mimetype: "audio/mp4" }, { quoted: m })
+        } catch ({ message }) {
+        return m.reply(message)
+        }
+        }
+        break
+        
         case "menu": {
         try {
-        let c = "_Hello i'm Shyzu simple WhatsApp bot created by MannR. I can to do something, search, get data and information only through WhatsApp._\n\n> *(__> ALL CMD <__)*\n> [ • ] .ai\n> [ • ] .clock\n> [ • ] .enc\n> [ • ] .menu\n> [ • ] .tiktok\n\n_© MannR - 2024_"
+        let c = "_Hello i'm Shyzu simple WhatsApp bot created by MannR. I can to do something, search, get data and information only through WhatsApp._\n\n> *(__> ALL CMD <__)*\n> [ • ] .ai\n> [ • ] .clock\n> [ • ] .enc\n> [ • ] .exec\n> [ • ] .menu\n> [ • ] .ngl\n> [ • ] .soundcloud\n> [ • ] .tiktok\n\n_© MannR - 2024_"
         m.reply(c)
         } catch ({ message }) {
         m.reply(message)
         }
         }
         break
+        default:
+        /** if (!shyzu.auto_ai[isSender].status) return
+        try {
+        var { data } = await axios({
+            "method": "GET",
+            "url": "https://hercai.onrender.com/v3/hercai",
+            "params": {
+                "question": text
+            }
+        })
+        m.reply(data.reply)
+        } catch ({ message }) {
+        return m.reply(message)
+        } **/
+    }
+    } catch ({ message }) {
+    console.log(chalk.redBright(message))
     }
 }
 
